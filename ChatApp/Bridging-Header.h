fa31467094c48fe659a6734ed21be50cb5fe968e
Reinder de Vries <reinder@interactionauts.nl>
2017-06-11 15:15:15 +0200

    Initial commit

---
+++ ChatApp/Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Bridging-Header.h
+//  ChatApp
+//
+//  Created by Reinder de Vries on 11/06/2017.
+//  Copyright © 2017 LearnAppMaking. All rights reserved.
+//
+
+#import <JSQMessagesViewController/JSQMessages.h>
+
+
---
+++ ChatApp/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  ChatApp
+//
+//  Created by Reinder de Vries on 11/06/2017.
+//  Copyright © 2017 LearnAppMaking. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct Constants
+{
+    struct refs
+    {
+        static let databaseRoot = FIRDatabase.database().reference()
+        static let databaseChats = databaseRoot.child("chats")
+    }
+}
